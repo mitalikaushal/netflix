d7e2b87925f583f22abbac9c3dc93b96ff2181d2
Mitali Kaushal <mitali.kaushal9@gmail.com>
2020-10-16 22:36:31 -0700

    created header signIn feature

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {OptForm} from '../components'
+import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faq";
@@ -6,9 +8,17 @@ import { FaqsContainer } from "../containers/faq";
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+      </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
+        <FaqsContainer />
+        <FooterContainer />
     </>
   );
 }
