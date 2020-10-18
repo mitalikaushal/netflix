6438c878bda3bc7fa7daaedf8193a7c6347baa23
Mitali Kaushal <mitali.kaushal9@gmail.com>
2020-10-18 01:44:57 -0700

    created custom hooks, listener, added protected routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home, Browse, Signin, Signup } from './pages';
-import * as ROUTES from './constants/routes';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Home, Browse, Signin, Signup } from "./pages";
+import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from './hooks';
 
 export default function App() {
+  const user = useAuthListener();
+
   return (
     <Router>
-<Route exact path={ROUTES.HOME}>
-    <Home />
-  </Route>
-  <Route exact path={ROUTES.BROWSE}>
-    <Browse/>
-  </Route>
-  <Route exact path="/signin">
-    <Signin/>
-  </Route>  
-  <Route exact path="signup">
-    <Signup/>
-  </Route>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <Signup />
+        </IsUserRedirect>
+
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
